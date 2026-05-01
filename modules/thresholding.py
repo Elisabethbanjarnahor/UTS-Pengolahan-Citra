0c45f91c82b6954f7a1e6a7e8b86df37d2f7a62c
Elisabethbanjarnahor <elisabethmarbun34@gmail.com>
2026-05-01 13:45:43 +0700

    Create thresholding.py

---
+++ modules/thresholding.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from utils.helper import tampilkan_hasil
+
+def demo_thresholding(img):
+    # ... [Paste isi fungsi demo_thresholding di sini] ...
+    pass
+
+def _hitung_otsu_manual(img):
+    # ... [Paste isi fungsi _hitung_otsu_manual di sini] ...
+    pass
